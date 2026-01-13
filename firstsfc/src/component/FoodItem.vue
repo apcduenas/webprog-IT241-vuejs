36bb81ce1cd1dd5649799742fd41d2e2c4a1d5a3
apcduenas <asduenas@student.apc.edu.ph>
2026-01-13 01:25:36 +0000

    Update

--- firstsfc/src/component/FoodItem.vue
+++ firstsfc/src/component/FoodItem.vue
@@ -1,22 +1,19 @@
 <template>
-  <h1>Fruits</h1>
-  <food-item />
-  <food-item />
-  <food-item />
+  <div>
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      name: 'Apples',
+      message: 'I like apples'
+    }
+  }
+};
+</script>
 
-<style>
-  #app > div {
-    border: dashed black 1px;
-    display: inline-block;
-    width: 120px;
-    margin: 10px;
-    padding: 10px;
-    background-color: lightgreen;
-  }
-  #app > div:hover {
-    cursor: pointer;
-  }
-</style>            
+<style></style>
