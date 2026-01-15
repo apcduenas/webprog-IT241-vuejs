379981ef6c2f41ad16b598aef8e3243c9f91e8ee
apcduenas <asduenas@student.apc.edu.ph>
2026-01-16 00:33:06 +0800

    TEST

--- home/js/vue.js
+++ home/js/vue.js
@@ -1,9 +1,53 @@
+
   const app = Vue.createApp({
-   data() {
+  data() {
     return {
-     message: "Happy New Year WEBPROG IT241!"
+      visitorName: '',
+      visitorComment: '',
+      isPublic: false,
+      guestbookEntries: [
+        { name: 'Sample Visitor', comment: 'Great site!', public: true, liked: false }
+      ],
+      images: [
+        { src: './images/landscape1.jpg', alt: 'Landscape 1' },
+        { src: './images/landscape2.jpg', alt: 'Landscape 2' },
+        { src: './images/Picture1.jpg', alt: 'Me 1' },
+        { src: './images/Picture2.jpg', alt: 'Me 2' }
+      ],
+      selected: null
     }
-   }
-  })
-  
-  app.mount('#app')
+  }, 
+  methods: {
+    addEntry() {
+      this.guestbookEntries.unshift({
+        name: this.visitorName,
+        comment: this.visitorComment,
+        public: this.isPublic,
+        liked: false
+      })
+      this.visitorName = ''
+      this.visitorComment = ''
+      this.isPublic = false
+    },
+    open(index) {
+      this.selected = index
+    },
+    close() {
+      this.selected = null
+    },
+    next() {
+      if (this.selected === null) return
+      this.selected = (this.selected + 1) % this.images.length
+    },
+
+    prev() {
+      if (this.selected === null) return
+      this.selected = (this.selected - 1 + this.images.length) % this.images.length
+    },
+    motivate() {
+      alert('Keep pushing forward and never give up on your dreams!');
+    }
+  }
+})
+
+app.mount('#app')
