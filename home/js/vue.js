6b5770680e1d9cf109bf4ea9546fce9117dba0b2
apcduenas <asduenas@student.apc.edu.ph>
2026-01-16 00:31:03 +0800

    ADD NEW

--- home/js/vue.js
+++ home/js/vue.js
@@ -1,53 +1,9 @@
-
   const app = Vue.createApp({
-  data() {
+   data() {
     return {
-      visitorName: '',
-      visitorComment: '',
-      isPublic: false,
-      guestbookEntries: [
-        { name: 'Sample Visitor', comment: 'Great site!', public: true, liked: false }
-      ],
-      images: [
-        { src: './images/landscape1.jpg', alt: 'Landscape 1' },
-        { src: './images/landscape2.jpg', alt: 'Landscape 2' },
-        { src: './images/Picture1.jpg', alt: 'Me 1' },
-        { src: './images/Picture2.jpg', alt: 'Me 2' }
-      ],
-      selected: null
+     message: "Happy New Year WEBPROG IT241!"
     }
-  }, 
-  methods: {
-    addEntry() {
-      this.guestbookEntries.unshift({
-        name: this.visitorName,
-        comment: this.visitorComment,
-        public: this.isPublic,
-        liked: false
-      })
-      this.visitorName = ''
-      this.visitorComment = ''
-      this.isPublic = false
-    },
-    open(index) {
-      this.selected = index
-    },
-    close() {
-      this.selected = null
-    },
-    next() {
-      if (this.selected === null) return
-      this.selected = (this.selected + 1) % this.images.length
-    },
-
-    prev() {
-      if (this.selected === null) return
-      this.selected = (this.selected - 1 + this.images.length) % this.images.length
-    },
-    motivate() {
-      alert('Keep pushing forward and never give up on your dreams!');
-    }
-  }
-})
-
-app.mount('#app')
+   }
+  })
+  
+  app.mount('#app')
