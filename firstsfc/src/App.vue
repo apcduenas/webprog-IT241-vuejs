24cdbbd3753387a80e1e662d640f1ccf30f921a9
apcduenas <asduenas@student.apc.edu.ph>
2026-01-17 14:06:34 +0800

    update

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,28 +1,11 @@
 <template>
-  <div class="Melon">>
 
-    <h2>{{ name }}</h2>
-    <p>{{ message }}</p>
-  </div>
+  <h1>Food</h1>
+  <food-item/>
+  <food-item2/>
+ 
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      name: 'Apple',
-      message: 'I want Apple'
-    }
-  }
-};
-</script>
+<script></script>
 
-<style>
-  .apple {
-    border: dashed black 1px;
-    display: inline-block;
-    margin: 10px;
-    padding: 10px;
-    background-color: red;
-  }
-</style>
+<style></style>
