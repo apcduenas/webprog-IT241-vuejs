d5db74df5d1770ce76dd313f9427bf7b348a5403
apcduenas <asduenas@student.apc.edu.ph>
2026-01-17 14:21:13 +0800

    update

--- js/vue.js
+++ js/vue.js
@@ -1,19 +1,9 @@
 const app = Vue.createApp({
-  data() {
+   data() {
     return {
-      message: "HAPPY NEW YEAR CLASSMATES!",
-      manyFoods: [
-        'https://www.w3schools.com/vue/img_burrito.svg',
-        'https://www.w3schools.com/vue/img_salad.svg',
-        'https://www.w3schools.com/vue/img_cake.svg',
-        'https://www.w3schools.com/vue/img_soup.svg',
-        'https://www.w3schools.com/vue/img_fish.svg',
-        'https://www.w3schools.com/vue/img_pizza.svg',
-        'https://www.w3schools.com/vue/img_rice.svg'
-      ]
+     message: "Hello Crush!"
     }
-  }
-})
-
-
-app.mount('#app')
+   }
+  })
+  
+  app.mount('#app')
