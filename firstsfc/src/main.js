9a57b7a1940bef6bb0ea5b713c2652867c034efb
apcduenas <asduenas@student.apc.edu.ph>
2026-01-19 17:46:52 +0800

    UPDATE

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-// Only keep the main App import. 
-// We handle the PersonalProfile import inside App.vue or here.
+import FoodItem from './components/FoodItem.vue'
+import FoodItem2 from './Components/FoodItem2.vue'
+import PersonalProfile from './Components/PersonalProfile.vue' // 1. Import it
 
 const app = createApp(App)
+
+app.component('food-item', FoodItem)
+app.component('food-item2', FoodItem2)
+app.component('personal-profile', PersonalProfile) // 2. Register it
+
 app.mount('#app')
