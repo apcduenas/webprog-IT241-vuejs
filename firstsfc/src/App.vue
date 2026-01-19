e0291deb5a544f95e17b05ca158e96dd58bd3154
apcduenas <asduenas@student.apc.edu.ph>
2026-01-19 16:26:53 +0800

    Check

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import PersonalProfile from "./components/Personalprofile.vue"
+import PersonalProfile from "./components/PersonalProfile.vue"
 import FoodItem from "./components/FoodItem.vue"
 import FoodItem2 from "./components/FoodItem2.vue"
 
