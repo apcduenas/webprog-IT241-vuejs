e85b2fb7e827f35ea7d68ce76a6a1bcccb2c9948
apcduenas <asduenas@student.apc.edu.ph>
2026-01-19 16:33:26 +0800

    TESSTTTTTT

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -5,9 +5,9 @@
 </template>
 
 <script>
-import PersonalProfile from "./components/Personalprofile.vue"
-import FoodItem from "./components/FoodItem.vue"
-import FoodItem2 from "./components/FoodItem2.vue"
+import PersonalProfile from './components/Personalprofile.vue'
+import FoodItem from './components/FoodItem.vue'
+import FoodItem2 from './components/FoodItem2.vue'
 
 export default {
   components: {
