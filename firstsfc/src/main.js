8520650c4042e45e14faa1e6883bb9891cbdced5
apcduenas <asduenas@student.apc.edu.ph>
2026-01-19 17:49:25 +0800

    UPDATE

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,13 +1,14 @@
 import { createApp } from 'vue'
+
 import App from './App.vue'
+import PersonalProfile from './components/PersonalProfile.vue'
 import FoodItem from './components/FoodItem.vue'
-import FoodItem2 from './Components/FoodItem2.vue'
-import PersonalProfile from './Components/PersonalProfile.vue' // 1. Import it
+import FoodItem2 from './components/FoodItem2.vue'
 
 const app = createApp(App)
 
+app.component('personal-profile', PersonalProfile)
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
-app.component('personal-profile', PersonalProfile) // 2. Register it
 
 app.mount('#app')
