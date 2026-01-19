124cc169b2b44a05313843108ad083704baf4360
apcduenas <asduenas@student.apc.edu.ph>
2026-01-19 17:39:53 +0800

    Test

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,21 +1,7 @@
 <template>
-  <PersonalProfile />
-  <FoodItem />
-  <FoodItem2 />
+  <personal-profile />
+  <hr>
+  <h1>Fruits</h1>
+  <food-item />
+  <food-item2 />
 </template>
-
-<script>
-import PersonalProfile from './components/Personalprofile.vue'
-import FoodItem from './components/FoodItem.vue'
-import FoodItem2 from './components/FoodItem2.vue'
-
-export default {
-  components: {
-    PersonalProfile,
-    FoodItem,
-    FoodItem2
-  }
-}
-</script>
-
-<style></style>
--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,14 +1,13 @@
 import { createApp } from 'vue'
-
 import App from './App.vue'
-import PersonalProfile from './components/Personalprofile.vue'
-import FoodItem from './components/FoodItem.vue'
-import FoodItem2 from './components/FoodItem2.vue'
+import FoodItem from './Components/FoodItem.vue'
+import FoodItem2 from './Components/FoodItem2.vue'
+import PersonalProfile from './Components/PersonalProfile.vue' // 1. Import it
 
 const app = createApp(App)
 
-app.component('personal-profile', Personalprofile)
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
+app.component('personal-profile', PersonalProfile) // 2. Register it
 
 app.mount('#app')
